8f84d9abdf29f089133cb99f3c01874ab1a95c0a
vahid lotfi <v.lotfi71@gmail.com>
2025-02-28 12:35:28 +0330

    reEdit project

--- internal/util/config.go
+++ internal/util/config.go
@@ -1,18 +1,11 @@
 package util
 
-import "github.com/spf13/viper"
+import "github.com/spf13/viper" // use viper for use env 
 
 type Config struct {
 	APPPORT  string `mapstructure:"APP_PORT"`
 	APPNAME  string `mapstructure:"APP_NAME"`
 	APPDEBUG string `mapstructure:"APP_DEBUG"`
-
-	DBCONNECTION string `mapstructure:"DB_CONNECTION"`
-	DBHOST       string `mapstructure:"DB_HOST"`
-	DBUSERNAME   string `mapstructure:"DB_USERNAME"`
-	DBPASSWORD   string `mapstructure:"DB_PASSWORD"`
-	DBDATABASE   string `mapstructure:"DB_DATABASE"`
-	DBPORT       string `mapstructure:"DB_PORT"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
@@ -21,13 +14,12 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName("app")
 
 	viper.AutomaticEnv()
-	err = viper.ReadInConfig()
+	err = viper.ReadConfig()
 	if err != nil {
 		return
 	}
 
-	err = viper.Unmarshal(&config)
+	viper.Unmarshal(&config)
 
 	return
-
 }
--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/vahidlotfi71/ticket/internal/util"
 )
 
 func main() {
-	// تنظیمات کانفیگ
-	config, err := util.LoadConfig(".")
+	config , err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal().Err(err).Msg("Connot load Config failed")
+		log.Fatal().Err(err).Msg("can not load config")
 	}
 
 	if config.APPDEBUG == "true" {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		log.logger = log.Output(zerolog.ConsoleWriter{out : os.Stderr})
 	}
 
-	// ارتباط با دیتا بیس
-
-	dsn := fmt.Sprintf("postgres;//%s:%s@%s:%s/%s?sslmode=disable",
-		config.DBUSERNAME,
-		config.DBPASSWORD,
-		config.DBHOST,
-		config.DBPORT,
-		config.DBDATABASE,
-	)
-	dbpool, err := pgxpool.New(context.Background(), dsn)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot to conncte database")
-	}
-	defer dbpool.Close()
-
-	
-
+	if config.APPNAME
 }
