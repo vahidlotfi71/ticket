abfe770f697eba0f5a6e99a396f145f9e917db00
vahid lotfi <v.lotfi71@gmail.com>
2025-07-14 23:04:50 +0330

    Define routes

--- Routes/Routes.go
+++ Routes/Routes.go
@@ -1,12 +1,23 @@
-package Routes
+package routes
 
 import "github.com/gofiber/fiber/v3"
 
 func Routes(app *fiber.App) {
-	adminGroup := app.Group("admin")
+	// گروه اصلی admin
+	adminGroup := app.Group("/admin")
+
 	adminGroup.Get("/", func(c fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
 			"message": "admin index route",
 		})
 	})
+
+	// گروه user زیر مجموعه‌ی admin
+	userGroup := adminGroup.Group("/user")
+
+	userGroup.Get("/", userIndex)             // لیست همه‌ی کاربران
+	userGroup.Get("/show/:id", userShow)      // نمایش جزئیات کاربر
+	userGroup.Post("/store", userStore)       // ساخت کاربر جدید
+	userGroup.Post("/update/:id", userUpdate) // ویرایش کاربر
+	userGroup.Post("/delete/:id", userDelete) // حذف کاربر
 }
