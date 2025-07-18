ea63f3ace74c760427058cfb3a20216f7cf1aafc
vahid lotfi <v.lotfi71@gmail.com>
2025-07-18 14:29:32 +0330

    Refactored store package

--- Controllers/Admin/UserController/Store.go
+++ Controllers/Admin/UserController/Store.go
@@ -1,30 +1,38 @@
 package UserController
 
 import (
+	"encoding/json"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/vahidlotfi71/ticket/Config"
 	"github.com/vahidlotfi71/ticket/Models"
 )
 
 func Store(c *fiber.Ctx) error {
-	user := new(Models.User)
-
-	// گرفتن داده‌های ورودی
-	if err := c.BodyParser(user); err != nil {
+	// Check if the request body is a valid json object
+	if !json.Valid(c.Body()) {
 		return c.Status(400).JSON(fiber.Map{
-			"message": "Invalid input",
+			"message": "invalid json encoding",
 		})
 	}
-
-	// ذخیره در دیتابیس
-	if err := Config.DB.Create(&user).Error; err != nil {
+	// Then trying to unmarshal/parse the json object
+	var data Models.User
+	if err := json.Unmarshal(c.Body(), &data); err != nil {
 		return c.Status(500).JSON(fiber.Map{
-			"message": "Error saving user",
+			"message": "could not parse the json",
 		})
 	}
-
-	return c.Status(201).JSON(fiber.Map{
-		"message": "User created successfully",
-		"data":    user,
+	// There is no need to specify the table, because we are
+	// using a model instance to create the record, so the gorm
+	// will figure it out by itself to handle it in the relative
+	// table in database
+	if err := Config.DB.Create(&data).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"message": "user created successfully",
 	})
+	/* if we want to handle it manually, we can use .Exec() method */
 }
