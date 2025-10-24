ed0ecd7e166aea9eb28ab7c6bc4db642515a5178
vahid lotfi <v.lotfi71@gmail.com>
2025-10-24 14:16:58 +0330

    created config for connect database

---
+++ Config/DB.go
@@ -0,0 +1,28 @@
+package Config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() error {
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		DB_USER,
+		DB_PASSWORD,
+		DB_HOST,
+		DB_PORT,
+		DB_NAME,
+	)
+
+	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	DB = db
+	return nil
+}
